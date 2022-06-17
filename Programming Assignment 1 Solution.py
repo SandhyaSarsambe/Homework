54e179b857b608863d110f01b1ced63de867ba1c
SandhyaSarsambe <105548150+SandhyaSarsambe@users.noreply.github.com>
2022-06-17 10:49:00 +0530

    Add files via upload

---
+++ Programming Assignment 1 Solution.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+#Q.1 Print Hello Python
+print("Hello Python")
+
+
+# In[19]:
+
+
+#Q.2 Addition and division
+a=5
+b=10
+c=a+b
+d=b/a
+print("The addition of no.s is",c)
+print("The division of no.s is", d)
+
+
+# In[17]:
+
+
+#Q.3 Area of traingle
+a=int(input('Enter the value for base:'))
+b=int(input('Enter the value for height'))
+c= 1/2*(a*b)
+print("The area of triangle is", c)
+
+
+# In[6]:
+
+
+#Q.4 Swap of two variables
+a='sudh'
+b='kumar'
+c=a
+a=b
+b=c
+print(a, b)
+
+
+# In[7]:
+
+
+#Q.5 Import random no.s
+import random
+num=random.random()
+print(num)
+
+
+# In[ ]:
+
+
+
+
